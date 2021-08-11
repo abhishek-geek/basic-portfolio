52bd7fab54f2a327cacb73d127172a7a0e78f00b
abhishek-geek <2019005@iiitdmj.ac.in>
2021-08-11 20:59:50 +0530

    added loader

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
+// hero = document.getElementById('hero');
+$(window).load(function() {
+	// Animate loader off screen
+	$(".se-pre-con").fadeOut("slow");;
+});
+
+
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
 	mobile_menu.classList.toggle('active');
