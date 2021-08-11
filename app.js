5c2905c6b6131b28f26f075b6cdb98639f50fd0d
abhishek-geek <2019005@iiitdmj.ac.in>
2021-08-11 22:56:29 +0530

    svg background and new loader

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
-// hero = document.getElementById('hero');
-$(window).load(function() {
-	// Animate loader off screen
-	$(".se-pre-con").fadeOut("slow");;
-});
+loader = document.getElementById('loader');
+hero = document.getElementById('loaded');
+
+window.addEventListener('load', ()=>{
+	console.log("loading");
+	loader.classList.remove("loading");
+	hero.setAttribute("id", "hero");
+})
+// $(window).load(function() {
+// 	// Animate loader off screen
+// 	$(".se-pre-con").fadeOut("slow");;
+// });
 
 
 hamburger.addEventListener('click', () => {
